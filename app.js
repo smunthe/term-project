aae2b5a4d411366d62d17b9fbd352fa889a3ee5f
Sam <samanthachombo45@gmail.com>
2025-05-24 21:02:36 -0700

    cleaned up unnecessary comments

--- app.js
+++ app.js
@@ -99,54 +99,6 @@ app.get('/about', (req, res) => {
 });
 
 
-//for nightmare it should be the same
-//going to do that when im free in a bit
-
-
-//for dream
-
-// app.get('/product[]', (req, res) => {
-//   res.render('product', {
-//     pageTitle: 'product name',
-//     productName: 'product name',
-//     productImage: '/assets/productImages/.png',
-//     productDescription: 'edit here',
-
-//     productPrice: 000000,
-
-//     stylesheet: '/productStyle.css' 
-//   });
-// });
-
-// Product page route
-
-// app.get('/product1', (req, res) => {
-//   res.render('product', {
-//     pageTitle: 'Dream House',
-//     productName: 'Dream House',
-//     // here goes the image just change after productImages
-//     productImage: '/assets/productImages/DreamHouse.png',
-//     //descption here any new proccut just copy and paste
-//     productDescription: 'edit here',
-
-//     productPrice: 20,
-
-//     stylesheet: '/productStyle.css'
-//   });
-// });
-
-
-// app.get('/product2', (req, res) => {
-//   res.render('product', {
-//     pageTitle: 'DreamCar',
-//     productName: 'DreamCar',
-//     productImage: '/assets/productImages/DreamCar.png',
-//     productDescription: 'edit here',
-//     stylesheet: '/productStyle.css' 
-//   });
-// });
-  
-
 
 
 
@@ -258,9 +210,6 @@ app.get('/product/:id', (req, res) => {
 });
 
 
-// app.get('/index', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
 
 app.get('/login', (req, res) => {
   res.render('login', {
